89672493b0a32705911d80529b37aea98c9ea3db
mingpeiw <mingpeiw@vmware.com>
2018-03-22 12:05:28 +0800

    add spy to http and httpclient

--- src/app/name.service.spec.ts
+++ src/app/name.service.spec.ts
@@ -1,8 +1,11 @@
+import { Observable } from 'rxjs/Observable';
 import { TestBed, getTestBed } from '@angular/core/testing';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { NameService } from './name.service';
 import { HttpModule, XHRBackend, Response, ResponseOptions, Headers, Http } from '@angular/http';
 import { MockBackend, MockConnection } from '@angular/http/testing';
+import 'rxjs/add/observable/of';
+import { HttpClient } from '@angular/common/http';
 
 describe(`NameService`, () => {
   let injector: TestBed;
@@ -21,13 +24,24 @@ describe(`NameService`, () => {
     nameService = injector.get(NameService);
   });
 
-  it('should test getUrl', () => {
+  it('should test getData', () => {
     nameService.getData().subscribe();
     const req = httpClientMock.expectOne('https://angular.io/guide/testing');
     expect(req.request.method).toBe('GET');
+    // req.flush({});  set response
   });
 
-  it('should test get', () => {
+  it('should test getData by spy', () => {
+    const httpClient = TestBed.get(HttpClient);
+    spyOn(httpClient, 'get').and.returnValue(Observable.of(new Response(new ResponseOptions({
+      body: 'succes',
+      headers: new Headers({sessionId: 'test succes'})
+    }))));
+    nameService.getData().subscribe();
+    expect(httpClient.get.calls.mostRecent().args[0]).toBe('https://angular.io/guide/testing');
+  });
+
+  it('should test getDataByHttp', () => {
     const httpMock: MockBackend = TestBed.get(XHRBackend);
     httpMock.connections.subscribe((connection: MockConnection) => {
       expect(connection.request.url).toBe('https://angular.io/guide/testing');
@@ -39,5 +53,17 @@ describe(`NameService`, () => {
     nameService.getDataByHttp().subscribe((res) => {
       expect(res.text()).toBe('succes');
     });
+  });
+
+  it('should test getDataByHttp by spy', () => {
+    const http = TestBed.get(Http);
+    spyOn(http, 'get').and.returnValue(Observable.of(new Response(new ResponseOptions({
+      body: 'succes',
+      headers: new Headers({sessionId: 'test succes'})
+    }))));
+    nameService.getDataByHttp().subscribe((res) => {
+      expect(res.text()).toBe('succes');
+    });
+    expect(http.get.calls.mostRecent().args[0]).toBe('https://angular.io/guide/testing');
   });
 });
